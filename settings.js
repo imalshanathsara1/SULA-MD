00f95f695ebee92c2e1a2dec39705496bf30c46d
imalshanathsara1 <124970468+imalshanathsara1@users.noreply.github.com>
2025-06-02 23:26:12 -0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? '𝐒𝐔𝐋𝐀-𝐌𝐃=5vsBxDSR#ObFA5QhiVQRUZi7H_Y6vHXICiFNPZ-SojJoO0Md-t4k' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? '𝐒𝐔𝐋𝐀-𝐌𝐃=UzEVTCYJ#FGeFQkdpuKawsidS34-Z_M2XUGZjzxJmrN955G5_xf4' : process.env.SESSION_ID,
 PREFIX: process.env.PREFIX || '.' ,
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS === undefined ?"true" : process.env.AUTO_READ_STATUS,
 MODE: process.env.MODE === undefined ?"privet" : process.env.MODE,
